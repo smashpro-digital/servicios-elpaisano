afa36b049968ad7102f1046a5053851108b3f26e
Maurice Jordan <maurice.jordan@me.com>
2025-11-10 14:56:02 -0500

    Create siteConfig.ts

---
+++ frontend/app/config/siteConfig.ts
@@ -0,0 +1,8 @@
+// app/config/siteConfig.ts
+export const siteConfig = {
+  businessName: "Servicios El Paisano, LLC",
+  phone: "+14232652528", // TODO: real number
+  phoneDisplay: "(423) 265-2528",
+  email: "servicioselpaisano@gmail.com",
+  address: "3501 Rossville Blvd, Chattanooga, TN 37407",
+};
