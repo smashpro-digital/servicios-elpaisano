d17b750ed3e43dc7a7e6d417faab2b760dc2a67c
Maurice Jordan <maurice.jordan@me.com>
2025-11-10 16:20:53 -0500

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: isProd ? "/servicios-elpaisano" : "",
+  assetPrefix: isProd ? "/servicios-elpaisano/" : "",
 };
 
 export default nextConfig;
