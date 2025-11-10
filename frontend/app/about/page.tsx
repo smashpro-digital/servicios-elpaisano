873200bc4ce0f68da99d15e7db1f35f5fac39299
Maurice Jordan <maurice.jordan@me.com>
2025-11-10 14:55:51 -0500

    Create page.tsx

---
+++ frontend/app/about/page.tsx
@@ -0,0 +1,22 @@
+export default function AboutPage() {
+  return (
+    <section className="page-section">
+      <h2>About Us</h2>
+        <p>
+            SERVICIOS EL PAISANO is an office that helps customer with translation 
+            and interpretation from Spanish to English or Vice versa with all the 
+            different need of the customer that lived in the Chattanooga and surround 
+            areas. SERVICIOS EL PAISANO provide more services to the Latin community.
+        </p>
+        <p>
+        SERVICIOS EL PAISANO was founded in November 2005 by Santos Chavez after graduated 
+        from Chattanooga State Technical Community College with Associated of Applied Science 
+        Degree in Information System Technology. Then Carmen Hernandez came in 2009 to help. 
+        Since 2009 they have worked together for better services. In addition have moved the 
+        office from one place to another for a friendly place to come in and great customer services.
+        </p>
+        <h2>Santos Chavez</h2>
+        <img src="/Santos-Chavez.jpg" alt="Santos Chavez" />
+    </section>
+  );
+}
