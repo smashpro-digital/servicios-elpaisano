e7e334f83552b0bdc502480fdb1b7ffc69cf44d2
Maurice Jordan <maurice.jordan@me.com>
2025-11-10 16:09:59 -0500

    out files

---
+++ _next/static/nA9BGqrtW-gXGDg_Z2MQj/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/servicios-elpaisano//_next/:path+",
+        "destination": "/servicios-elpaisano/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
