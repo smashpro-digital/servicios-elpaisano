b6385ef598c2e2370aec852203e83b05939db9d8
Maurice Jordan <maurice.jordan@me.com>
2025-12-03 09:51:53 -0500

    studio build
    aab build

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,14 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config = {
+  appId: "com.servicioselpaisano.app",
+  appName: "Servicios El Paisano",
+  webDir: "public",
+  server: {
+    url: "https://servicioselpaisano.com",
+    cleartext: true
+  }
+
+};
+
+export default config;
